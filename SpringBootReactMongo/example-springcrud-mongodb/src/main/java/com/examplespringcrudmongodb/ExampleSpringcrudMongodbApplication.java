250fe7e574a0b9e26ffe8ce3976560751ea2ef83
rohan <rohan@gmail.com>
2022-11-07 21:05:37 +0530

    done

---
+++ SpringBootReactMongo/example-springcrud-mongodb/src/main/java/com/examplespringcrudmongodb/ExampleSpringcrudMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.examplespringcrudmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringcrudMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringcrudMongodbApplication.class, args);
+	}
+
+}
---
+++ SpringBootReactMongo/example-springcrud-mongodb/src/main/java/com/examplespringcrudmongodb/controller/BlogController.java
@@ -0,0 +1,51 @@
+package com.examplespringcrudmongodb.controller;
+
+import com.examplespringcrudmongodb.model.Blog;
+import com.examplespringcrudmongodb.service.IBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+public class BlogController {
+
+    @Autowired
+    private IBlogService blogService;
+
+    @GetMapping("/all")
+    public List<Blog> getAllBlogs(){
+        return blogService.findAll();
+    }
+
+    @GetMapping("/title/{title}")
+    public List<Blog> getBlogsByTitle(@PathVariable String title){
+        return blogService.findByTitle(title);
+    }
+
+    @GetMapping("/id/{id}")
+    public Blog getBlogsById(@PathVariable String id){
+        return blogService.findById(id);
+    }
+
+    @PostMapping("/create")
+    public Blog create(@RequestBody Blog blog){
+        blog.setBlogId(UUID.randomUUID().toString());
+        return blogService.create(blog);
+    }
+
+    @PutMapping("/update")
+    public Blog update(@RequestBody Blog blog){
+        return blogService.update(blog);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteById(@PathVariable String id){
+        blogService.delete(id);
+    }
+
+}
